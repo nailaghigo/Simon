2746e5c14ff5774c9190d6b742c09f75be295fa2
Naila Ghigo <nailaghigo20@gmail.com>
2023-08-09 12:54:35 -0300

    Update logic to make Simon play 10 sequences, add a level counter and disable start button while the game is on

--- js/index.js
+++ js/index.js
@@ -1,36 +1,55 @@
-var simonSequence = [];
-var gamerSequence = [];
-var counterLevel = 0;
-
+var colors = ['red', 'green', 'blue', 'yellow']
+var simonSequence = []
+var gamerSequence = []
+var counterLevel = 0
+var isGameStarted = false
 
 var startGameButton = document.querySelector('.start-button')
+var square = document.querySelector('.container')
 
 function nextColor() {
-  var colors = ['red', 'green', 'blue', 'yellow']
-  var randomColor = colors[Math.floor(Math.random() * colors.length)]
+  var index = Math.floor(Math.random() * colors.length)
+  var color = colors[index]
 
-  return randomColor
+  return {index, color}
 }
 
-function highlightedColor(color) {
-  var color = document.querySelector(`#${color}`)
+function highlightedColor(i, color) {
+  setTimeout(function () {
+    var selectedColor = document.querySelector('#' + color)
+    selectedColor.classList.add('highlighted')
 
-  color.classList.add('highlighted')
+    setTimeout(function(){
+      selectedColor.classList.remove('highlighted')
 
-  setTimeout(function(){
-    color.classList.remove('highlighted')
-  },500)
+      if (i === simonSequence.length - 1) {
+        nextLevel()
+      }
+    }, 500)
+  }, (i + 1) * 600)
 }
 
 function nextLevel() {
-  counterLevel += 1;
-  var nextSequence = [...simonSequence]
-
-  nextSequence.push(nextColor());
+  counterLevel += 1
+  document.querySelector('.level-counter').innerHTML = counterLevel
+  nextRound()
 }
 
-function startGame()  {
-  startGameButton.disabled = true;
+function nextRound()  {
+  var color = nextColor()
+
+  if (!isGameStarted){
+    isGameStarted = true
+    startGameButton.disabled = true
+  }
+
+  if(counterLevel === 10) return false
+
+  simonSequence.push(color)
+
+  for (var i = 0; i < simonSequence.length; i++) {
+    highlightedColor(i, simonSequence[i].color)
+  }
 }
 
-startGameButton.addEventListener('click', startGame)
+startGameButton.addEventListener('click', nextRound)
