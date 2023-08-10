41faec4e2c42237572e532f0c6194071e12ef2df
Naila Ghigo <nailaghigo20@gmail.com>
2023-08-10 10:50:08 -0300

    Identify turns and add matching colors validation

--- js/index.js
+++ js/index.js
@@ -1,15 +1,44 @@
 var colors = ['red', 'green', 'blue', 'yellow']
 var simonSequence = []
-var gamerSequence = []
+var playerClickCount = 0
 var counterLevel = 0
 var isGameStarted = false
+var currentPlayer = 'computer'
 
 var startGameButton = document.querySelector('.start-button')
 var square = document.querySelector('.container')
 
 function handlePlayerClick (e) {
-  var squareColor = e.target.classList[1]
-  console.log('color', squareColor)
+  if (currentPlayer === 'player') {
+    playerClickCount++
+    var squareColor = e.target.classList[1]
+
+    if (simonSequence[playerClickCount - 1].color !== squareColor) {
+      // END GAME
+      alert("Game Over! Try again.");
+      simonSequence = []
+      playerClickCount = 0
+      counterLevel = 0
+      isGameStarted = false
+      startGameButton.disabled = false
+      currentPlayer = 'computer'
+    }
+
+    if (playerClickCount === simonSequence.length) {
+      currentPlayer = "computer"; // Cambia el turno a la computadora
+      setTimeout(nextRound, 1000);
+    }
+  }
+}
+
+function checkPattern() {
+  for (var i = 0; i < playerSequence.length; i++) {
+
+    if (playerSequence[i] !== simonSequence[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function nextColor() {
@@ -20,6 +49,10 @@ function nextColor() {
 }
 
 function highlightedColor(i, color) {
+  if (i === simonSequence.length - 1) {
+    nextLevel()
+  }
+
   setTimeout(function () {
     var selectedColor = document.querySelector('#' + color)
     selectedColor.classList.add('highlighted')
@@ -27,9 +60,6 @@ function highlightedColor(i, color) {
     setTimeout(function(){
       selectedColor.classList.remove('highlighted')
 
-      if (i === simonSequence.length - 1) {
-        nextLevel()
-      }
     }, 500)
   }, (i + 1) * 600)
 }
@@ -37,23 +67,27 @@ function highlightedColor(i, color) {
 function nextLevel() {
   counterLevel += 1
   document.querySelector('.level-counter').innerHTML = counterLevel
+  currentPlayer = currentPlayer === 'computer' ? 'player' : 'computer'
+
   nextRound()
 }
 
 function nextRound()  {
-  var color = nextColor()
-
   if (!isGameStarted){
     isGameStarted = true
     startGameButton.disabled = true
   }
 
-  if(counterLevel === 10) return false
+  if (currentPlayer === 'computer'){
+    var color = nextColor()
 
-  simonSequence.push(color)
+    simonSequence.push(color)
 
-  for (var i = 0; i < simonSequence.length; i++) {
-    highlightedColor(i, simonSequence[i].color)
+    for (var i = 0; i < simonSequence.length; i++) {
+      highlightedColor(i, simonSequence[i].color)
+    }
+
+    playerClickCount = 0
   }
 }
 
